082f2db7c08a8fa3e19c46df0b4b5b0a5f81d1cd
= <=>
2021-06-16 17:45:58 -0400

    NLP with Deep Learning

---
+++ preprocess_data.py
@@ -0,0 +1,44 @@
+import numpy as np
+# from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import PorterStemmer
+from num2words import num2words
+
+# import nltk
+# nltk.download()
+
+
+def convert_lower_case(data):
+    return np.char.lower(data)
+
+
+def remove_punctuation(data):
+    symbols = "!\"#$%&()*+-./:;<=>?@[\]^_`{|}~\n"
+    for i in range(len(symbols)):
+        data = np.char.replace(data, symbols[i], ' ')
+        data = np.char.replace(data, "  ", " ")
+    data = np.char.replace(data, ',', '')
+    return data
+
+def remove_apostrophe(data):
+    return np.char.replace(data, "'", "")
+
+
+def convert_numbers(data):
+    tokens = word_tokenize(str(data))
+    new_text = ""
+    for w in tokens:
+        try:
+            w = num2words(int(w))
+        except:
+            a = 0
+        new_text = new_text + " " + w
+    new_text = np.char.replace(new_text, "-", " ")
+    return new_text
+
+def preprocess(data):
+    data = convert_lower_case(data)
+    data = remove_punctuation(data)
+    data = remove_apostrophe(data)
+    data = convert_numbers(data)
+    return str(data)
